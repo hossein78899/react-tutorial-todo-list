6c19c9264e21f78a11d91004fcd64c9a74f18f34
hossein78899 <61446842+hossein78899@users.noreply.github.com>
2021-08-08 00:53:22 +0300

    finish

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class todoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
-      <div>
-        Hello from todoInput
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-text bg-primary text-white">
+              <i className="fas fa-book"></i>
+            </div>
+            <input
+              type="text"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+              className="form-control text-capitalize"
+            />
+          </div>
+          <div className="d-grid gap-2">
+            <button
+              disabled={item ? false : true}
+              className={
+                editItem
+                  ? "btn btn-success mt-3"
+                  : "btn btn-primary mt-3 text-uppercase"
+              }
+              type="submit"
+            >
+              {editItem ? "edite item" : "add item"}
+            </button>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
-import Item from "./TodoItem";
+import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
   render() {
-    return <section>
-      <h2>todo list</h2>
-      <Item/>
-    </section>;
+    const { items, handleClearList, handleDelete, handleEdit } = this.props;
+
+    return (
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">tod list</h3>
+        {items.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              handleDelete={() => handleDelete(item.id)}
+              title={item.title}
+              handleEdit={() => handleEdit(item.id)}
+            ></TodoItem>
+          );
+        })}
+        <div className="d-grid  gap-2">
+          <button
+            className="btn btn-danger text-uppercase mt-5"
+            type="button"
+            onClick={handleClearList}
+          >Clear list</button>
+        </div>
+      </ul>
+    );
   }
 }
